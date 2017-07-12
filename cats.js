662dd106f1baf03ed52f350f8a6336676dc29f88
DocPrior <Sara Prior>
2017-07-12 15:13:19 -0400

    add other 2 cat boxes, finish assignment

--- cats.js
+++ cats.js
@@ -2,19 +2,28 @@ document.addEventListener('DOMContentLoaded', function() {
   var catButton = document.querySelector('.summon-cats');
   var box1 = document.querySelector('#cat1');
   var box2 = document.querySelector('#cat2');
-  var box3 = document.querySelector('#cats3');
+  var box3 = document.querySelector('#cat3');
   catButton.addEventListener('click', function() {
     $.ajax({
       url: 'http://bitkittens.herokuapp.com/cats.json',
       method: 'GET',
       dataType: 'json'
     }).done(function(responseData) {
-      var catPic = document.createElement('img');
-      catPic.setAttribute('src', responseData.cats[0].photo);
-      catPic.setAttribute('alt', responseData.cats[0].name);
-      box1.append(catPic);
+      var catPic1 = document.createElement('img');
+      catPic1.setAttribute('src', responseData.cats[0].photo);
+      catPic1.setAttribute('alt', responseData.cats[0].name);
+      box1.append(catPic1);
+      var catPic2 = document.createElement('img');
+      catPic2.setAttribute('src', responseData.cats[1].photo);
+      catPic2.setAttribute('alt', responseData.cats[1].name);
+      box2.append(catPic2);
+      var catPic3 = document.createElement('img');
+      catPic3.setAttribute('src', responseData.cats[2].photo);
+      catPic3.setAttribute('alt', responseData.cats[2].name);
+      box3.append(catPic3);
     });
   });
+  //I know that it needs to be refactored.
 
 
 });
