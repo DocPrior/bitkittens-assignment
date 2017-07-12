467eb1a4ff519ba7813f22204c795e2a3da5f41d
DocPrior <Sara Prior>
2017-07-12 15:05:10 -0400

    first cat

--- cats.js
+++ cats.js
@@ -1,11 +1,19 @@
 document.addEventListener('DOMContentLoaded', function() {
   var catButton = document.querySelector('.summon-cats');
+  var box1 = document.querySelector('#cat1');
+  var box2 = document.querySelector('#cat2');
+  var box3 = document.querySelector('#cats3');
   catButton.addEventListener('click', function() {
     $.ajax({
       url: 'http://bitkittens.herokuapp.com/cats.json',
       method: 'GET',
       dataType: 'json'
-    })
+    }).done(function(responseData) {
+      var catPic = document.createElement('img');
+      catPic.setAttribute('src', responseData.cats[0].photo);
+      catPic.setAttribute('alt', responseData.cats[0].name);
+      box1.append(catPic);
+    });
   });
 
 
